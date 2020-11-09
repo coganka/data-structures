54238daafffdc2802969d7bbe6fcc902831f7957
Can Ogan Karagün <36695970+coganka@users.noreply.github.com>
2020-11-09 15:56:55 +0300

    BST update

---
+++ bst.js
@@ -0,0 +1,62 @@
+class Node {
+    constructor(value) {
+        this.left = null;
+        this.right = null;
+        this.value = value;
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+        if (!this.root) {
+            this.root = newNode;
+        } else {
+            let currentNode = this.root;
+            while(true) {
+                if (value < currentNode.value) {
+                    if (!currentNode.left) {
+                        currentNode.left = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.left;
+                } else {
+                    if (!currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.right; 
+                }
+            } 
+        }
+    }
+
+    lookup(value) {
+        if (!this.root) {
+            return false;
+        }
+        let currentNode = this.root;
+        while (currentNode) {
+            if (value < currentNode.value) {
+                currentNode = currentNode.left;
+            } else if (value > currentNode.value) {
+                currentNode = currentNode.right;
+            } else if (value === currentNode.value) {
+                return currentNode;
+            }
+        }
+        return false;
+    }
+}
+
+const myBST = new BST();
+myBST.insert(10);
+myBST.insert(5);
+myBST.insert(22);
+
+//console.log(myBST.lookup(5));
+console.log(myBST);
