a0c800ba33d3d41a7ebb3b1b2f61d008befe858c
Can Ogan Karagün <36695970+coganka@users.noreply.github.com>
2020-11-09 16:57:49 +0300

    DFS update

--- bst.js
+++ bst.js
@@ -70,13 +70,68 @@ class BST {
         }
         return list;
     }
+
+    dfsPreOrder() {
+        var list = [];
+        var current = this.root;
+        function traverse(node) {
+            list.push(node.value);
+            if (node.left) {
+                traverse(node.left);
+            }
+            if (node.right) {
+                traverse(node.right);
+            }
+        }
+        traverse(current);
+        return list;
+    }
+
+    dfsInOrder() {
+        var list = [];
+        var current = this.root;
+        function traverse(node) {
+            if (node.left) {
+                traverse(node.left);
+            }
+            list.push(node.value);
+            if (node.right) {
+                traverse(node.right);
+            }
+        }
+        traverse(current);
+        return list;
+    }
+
+    dfsPostOrder() {
+        var list = [];
+        var current = this.root;
+        function traverse(node) {
+            if (node.left) {
+                traverse(node.left);
+            }
+            if (node.right) {
+                traverse(node.right);
+            }
+            list.push(node.value);
+        }
+        traverse(current);
+        return list;
+    }
 }
 
 const myBST = new BST();
 myBST.insert(10);
 myBST.insert(5);
-myBST.insert(22);
+myBST.insert(30);
+myBST.insert(2);
+myBST.insert(16);
+myBST.insert(78);
+myBST.insert(9);
 
 //console.log(myBST.lookup(5));
 //console.log(myBST);
-console.log(myBST.bfs());
+//console.log(myBST.bfs());
+console.log(myBST.dfsPreOrder());
+console.log(myBST.dfsInOrder());
+console.log(myBST.dfsPostOrder());
